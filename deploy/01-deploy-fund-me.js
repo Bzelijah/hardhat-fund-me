92aef41df6799346172d07659c397c28e7103f71
bzelijah <ilya.andreev@onlinesup.com>
2022-11-06 18:23:10 +0300

    add verify function

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,5 +1,6 @@
 const { network } = require("hardhat");
 const { networkConfig, developmentChains } = require('../helper-hardhat-config');
+const { verify } = require('../utils/verify');
 
 module.exports.default = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
@@ -14,11 +15,17 @@ module.exports.default = async ({ getNamedAccounts, deployments }) => {
         ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
     }
 
+    const args = [ethUsdPriceFeedAddress];
     const fundMe = await deploy("FundMe", {
        from: deployer,
-       args: [ethUsdPriceFeedAddress],
+       args,
        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
     });
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        await verify(fundMe.address, args);
+    }
     log("_________________________________________________");
 };
 
