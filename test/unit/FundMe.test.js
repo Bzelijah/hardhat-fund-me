6ad23da35beefda30b0d564035a5814494de01d3
bzelijah <ilya.andreev@onlinesup.com>
2022-11-17 14:17:20 +0800

    add test for fund and withdraw functions

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -7,7 +7,6 @@ describe("FundMe", async () => {
    let mockV3Aggregator;
    const sendValue = ethers.utils.parseEther("1");
    beforeEach(async () => {
-      // const accounts = await ethers.getSigners();
       deployer = (await getNamedAccounts()).deployer;
       await deployments.fixture(["all"]);
       fundMe = await ethers.getContract("FundMe", deployer);
@@ -28,10 +27,36 @@ describe("FundMe", async () => {
 
       it("updated the amount funded data structure ", async () => {
          await fundMe.fund({ value: sendValue });
-         const response = await fundMe.addressToAmountFunded(
-             deployer,
-         );
+         const response = await fundMe.addressToAmountFunded(deployer);
          assert.equal(response.toString(), sendValue.toString());
+      });
+
+      it('adds funder add to array funders', async () => {
+         await fundMe.fund({ value: sendValue });
+         const funder = await fundMe.funders(0);
+         assert.equal(funder, deployer);
+      });
+   });
+
+   describe("withdraw", async () => {
+      beforeEach(async () => {
+         await fundMe.fund({ value: sendValue });
+      });
+
+      it ('withraw ETH from a single founder', async () => {
+         const starterFundMeBalance = await fundMe.provider.getBalance(fundMe.address);
+         const startingDeployerBalance = await fundMe.provider.getBalance(deployer);
+
+         const transactionResponse = await fundMe.withdraw();
+         const { gasUsed, effectiveGasPrice } = await transactionResponse.wait(1);
+         const gasCost = gasUsed.mul(effectiveGasPrice)
+
+         const endingFundMeBalance = await fundMe.provider.getBalance(fundMe.address);
+         const endingDeployerBalance = await fundMe.provider.getBalance(deployer);
+
+         assert.equal(endingFundMeBalance, 0);
+         assert.equal(starterFundMeBalance.add(startingDeployerBalance), endingDeployerBalance.add(gasCost).toString());
+
       });
    });
 });
